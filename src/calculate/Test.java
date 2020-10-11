6dd6f5da7c621c5c04d30a13d42e1480879bc247
deer <1184683288@qq.com>
2020-10-11 17:22:07 +0800

    Test1

---
+++ src/calculate/Test.java
@@ -0,0 +1,27 @@
+package calculate;
+
+/*public class Test {
+	public static void main(String args[]){
+		BinaryTree bTree;
+		for(int i = 0; i < 10; i++){
+			bTree = new BinaryTree(2);
+			bTree.createBTree();
+			String result = bTree.CalAndVal();
+			System.out.println(bTree.toString() + "=" + result);
+		}
+	}
+}*/
+
+public class Test{
+	public static void main(String[] args){
+    //BinaryTree binaryTree=new BinaryTree(3);
+    //binaryTree.createBTree(10,1);
+    CreatSubjects creatSubjects=new CreatSubjects();
+   
+  
+    	
+    	    creatSubjects.creatSubject(10000,1);
+		
+	
+	}
+}
