78515c608fa0c039281a6ccfbed0a7966f0a876b
deer <1184683288@qq.com>
2020-10-11 17:13:25 +0800

    BinaryList1

---
+++ src/calculate/BinaryList.java
@@ -0,0 +1,11 @@
+package calculate;
+
+import java.util.ArrayList;
+
+public class BinaryList {
+    public static ArrayList<String> saveList = new ArrayList<>();
+
+    public static void saveBinary(String string) {
+        saveList.add(string);
+    }
+}
